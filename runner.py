4ccc9a071b2caf4f2f48fefe24d694a07e8554d9
Jonathan Kirtland <42749198+jonathankirtland@users.noreply.github.com>
2023-02-12 23:10:54 -0800

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+servy = Service('./chromedriver')
+driver = webdriver.Chrome(service=servy)
+path = 'https://learn.zybooks.com/signin'
+driver.get(path)
+
+zy_login_email = driver.find_element(By.ID, "ember8")
+zy_login_pw = driver.find_element(By.ID, "ember10")
+zy_submit_btn = driver.find_element(By.ID, "ember6")
+
+zy_login_email.send_keys("jonathan.kirtland@student.csulb.edu")
+
+zy_login_pw.send_keys("Ripley0614")
+
+zy_login_pw.send_keys(Keys.ENTER)
+
+zy_341_section_access_btn = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.ID, "ember21")))
+zy_341_section_access_btn.click()
+
+
+zy_assignment_tab_btn = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"ember101\"]/div[2]/button[3]")))
+zy_assignment_tab_btn.click()
+
+zy_assignment_1_menu_click = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"ember101\"]/div[1]/div[3]/div/div[1]/h3")))
+zy_assignment_1_menu_click.click()
+
+
+
+
+
+input()
